b68d436715ec1692d8e293043cb0d25ef592e721
iancmy <103072306+iancmy@users.noreply.github.com>
2022-08-09 18:01:46 +0800

    Navbar update on scroll effect.

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+// make nav-container opaque when scrolling
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav-container');
+    const navHeight = parseInt(getComputedStyle(nav).getPropertyValue('height'));
+
+    if (window.scrollY > navHeight) {
+        nav.classList.add('opaque');
+    } else {
+        nav.classList.remove('opaque');
+    }
+})
+
+
+
